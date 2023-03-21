7cdbf2d9cd50153991f959ade3ead8ecb8d0b108
Thomas Boerger <thomas@webhippie.de>
2023-03-21 12:32:19 +0100

    chore: integrate initial formulas

---
+++ Formula/gopad-api.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+# Definition of the gopad-api formula
+class GopadApi < Formula
+  desc "Gopad: API"
+  homepage "https://gopad.eu"
+  license "Apache-2.0"
+
+  url "https://github.com/gopad/gopad-api/archive/v0.0.0.tar.gz"
+  sha256 ""
+  head "https://github.com/gopad/gopad-api.git", branch: "master"
+
+  test do
+    system bin / "gopad-api", "--version"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ENV["SHA"] = "undefined"
+    ENV["VERSION"] = if build.head?
+                       Utils.git_short_head(length: 8)
+                     else
+                       url.split("/").last.gsub(".tar.gz", "").gsub("v", "")
+                     end
+
+    system "make", "generate", "build"
+    bin.install "bin/gopad-api"
+
+    FileUtils.touch("gopad-api.conf")
+    etc.install "gopad-api.conf"
+  end
+
+  def post_install
+    (var / "gopad-api").mkpath
+  end
+
+  service do
+    run [opt_bin / "gopad-api", "server"]
+    environment_variables GOPAD_API_ENV_FILE: etc / "gopad-api.conf"
+    keep_alive true
+    log_path var / "log/gopad-api.log"
+    error_log_path var / "log/gopad-api.log"
+  end
+end
---
+++ Formula/gopad-cli.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Definition of the gopad-cli formula
+class GopadCli < Formula
+  desc "Gopad: CLI"
+  homepage "https://gopad.eu"
+  license "Apache-2.0"
+
+  url "https://github.com/gopad/gopad-cli/archive/v0.0.0.tar.gz"
+  sha256 ""
+  head "https://github.com/gopad/gopad-cli.git", branch: "master"
+
+  test do
+    system bin / "gopad-cli", "--version"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ENV["SHA"] = "undefined"
+    ENV["VERSION"] = if build.head?
+                       Utils.git_short_head(length: 8)
+                     else
+                       url.split("/").last.gsub(".tar.gz", "").gsub("v", "")
+                     end
+
+    system "make", "generate", "build"
+    bin.install "bin/gopad-cli"
+  end
+end
